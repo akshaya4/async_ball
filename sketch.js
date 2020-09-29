b756e07f77cdd90f447479ddac5c493c38270736
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-29 14:33:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,20 +12,20 @@ function setup(){
 }
 
 function draw(){
-    background("white");
+  background("white");
+  
     if(keyDown(LEFT_ARROW)){
-     //   writePosition(-1,0);
+      writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-     //   writePosition(1,0);
+      writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-     //   writePosition(0,-1);
+      writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-      //  writePosition(0,+1);
+      writePosition(0,+1);
     }
-    drawSprites();
 }
 
 function writePosition(x,y){
